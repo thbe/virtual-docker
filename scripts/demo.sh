b4644b50c2fd78624bc95d304483a0499b91e5c4
thbe <project@bendler-net.de>
2016-06-22 10:35:51 +0200

    Prepare v1.0.0

--- scripts/demo.sh
+++ scripts/demo.sh
@@ -3,21 +3,37 @@
 # Simple docker demo to show possibilities
 #
 # Start docker container with:
-# docker run --rm -ti -p 80:80 local/centos7 /bin/bash
+# docker run --rm -ti -p 80:80 local/centos6 /bin/bash
+# or
+# docker run --rm -ti -v /sys/fs/cgroup:/sys/fs/cgroup:ro -p 80:80 -p 80:80 local/centos7 /bin/bash
 #
+# Start demo with:
+# wget https://raw.githubusercontent.com/thbe/virtual-docker/master/scripts/demo.sh
+# chmod 755 /demo.sh
+# /demo.sh
+#
+
+### Get current operating system major version ###
+MAJORVER=$(cat /etc/redhat-release | sed -e 's/.*release\ //' -e 's/\ .*//' | cut -d '.' -f1)
 
 ### Install webserver ###
 yum -y install nginx
-service nginx start
+if [ "x${MAJORVER}" = "x6" ]; then
+  service nginx start
+fi
+if [ "x${MAJORVER}" = "x7" ]; then
+  systemctl start nginx.service
+fi
+
 
 ### Install puppet ###
 yum -y install puppet
-puppet module install thbe/ssmtp
-puppet apply -e "include ssmtp"
+puppet module install thbe-ssmtp
+puppet apply -e 'class { "ssmtp": }'
 
 ### Show demo results ###
 echo
-echo "Webserver installed, please check http://$(facter ipaddress)/"
+echo "Webserver installed, please check http://localhost/"
 echo "Puppet installed ssmtp and switched the default mta:"
 ls -alFh /usr/sbin/sendmail
 ls -alFh /etc/alternatives/mta
